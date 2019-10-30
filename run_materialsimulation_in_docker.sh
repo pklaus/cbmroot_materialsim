55bce87871de6811764ad34e074177146e532a71
Philipp Klaus <philipp.l.klaus@web.de>
2019-10-30 18:16:04 +0100

    split running simulation and analysis into two steps

--- run_materialsimulation_in_docker.sh
+++ run_materialsimulation_in_docker.sh
@@ -6,15 +6,22 @@ RESULT_IMAGE=materialsim_result
 docker run \
   --name $CONTAINER \
   pklaus/cbmroot_materialsim:DEC17 \
-  "source ~/.bashrc; cd /cbmroot/macro/mvd; root -b ./materialsimulation.c; root -b ./materialanalysis.C"
+  "source ~/.bashrc; cd /cbmroot/macro/mvd; root -b ./materialsimulation.c"
 
+docker commit $CONTAINER $RESULT_IMAGE
+
+docker run \
+  --name $CONTAINER \
+  $RESULT_IMAGE \
+  "source ~/.bashrc; cd /cbmroot/macro/mvd; root -b ./materialanalysis.C"
+
+docker commit $CONTAINER $RESULT_IMAGE
 
 for FILE in Sim_Box_1MEvents_mvd_15a_360phi.{params,mc,radlen}.root
 do
   docker cp $CONTAINER:/cbmroot/macro/mvd/$FILE ./output/
 done
 
-docker commit $CONTAINER $RESULT_IMAGE
 docker rm $CONTAINER
 
 echo "you can run    docker run --rm -it $RESULT_IMAGE /bin/bash    to analyse the result inside of a container"
